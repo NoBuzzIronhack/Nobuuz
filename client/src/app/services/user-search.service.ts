88188c2ce1efdfc4890a49dc67b93aac13a50562
Tess <tessdegregorio@gmail.com>
2017-12-14 15:40:03 +0100

    Fixed error and added environment to services

--- client/src/app/services/user-search.service.ts
+++ client/src/app/services/user-search.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment'
 
-
-const DOMAIN = "http://localhost:3000";
+const DOMAIN = environment.baseurl;
 const PATH = "/user-search";
 const BASEURL = `${DOMAIN}${PATH}`;
 
